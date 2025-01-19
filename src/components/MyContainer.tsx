df1f20e0edd03ca2b044a43e5ddf73040b8707e3
roopemyller <roope.myller@gmail.com>
2025-01-19 14:37:43 +0200

    task 5

--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -3,9 +3,7 @@ import MyList, { Item } from './MyList'
 
 function MyContainer() {
     const [items, setItems] = useState<Item[]>([
-        {id: "1", text: "Semmonen"},
-        {id: "2", text: "Tämmönen"},
-        {id: "3", text: "Tuommonen"},
+        {id: "1", text: "Semmonen", clicked: false},
     ])
 
     const [newItemText, setNewItemText] = useState<string>("")
@@ -15,10 +13,17 @@ function MyContainer() {
             const newItem: Item = {
                 id: (items.length + 1).toString(),
                 text: newItemText,
+                clicked: false
             }
             setItems([...items, newItem])
             setNewItemText("")
         }
+    }
+
+    function updateList(id: string) {
+        setItems(items.map(item =>
+            item.id === id ? { ...item, clicked: !item.clicked } : item
+        ))
     }
 
     return (
@@ -29,7 +34,7 @@ function MyContainer() {
                 placeholder='Enter new item text'
             />
             <button onClick={addItem}>Add item</button>
-            <MyList header="My List" items={items}/>
+            <MyList header="My List" items={items} updateList={updateList}/>
         </div>
     )
 }
--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,20 +1,28 @@
 
 export type Item = {
     id: string,
-    text: string
+    text: string,
+    clicked: boolean
 }
 
 interface ListProps {
     header: string
     items: Item[]
+    updateList: (id: string) => void
 }
 
-function MyList( {header, items}: ListProps ) {
+function MyList( {header, items, updateList}: ListProps ) {
   return (
     <ol>
         <h2>{header}</h2>
         {items.map((item) => (
-            <li key={item.id}>{item.text}</li>
+            <li 
+                key={item.id}
+                onClick={() => updateList(item.id)}
+                style={{textDecoration: item.clicked ? 'line-through' : 'none'}}
+            >
+                {item.text}
+            </li>
         ))}
     </ol>
   )
